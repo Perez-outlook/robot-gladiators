be8217bdcb4d292a4db4797cfe2cd35990b1530e
dianepj12@gmail.com <dianepj12@gmail.com>
2021-10-28 02:36:51 -0500

    almost done idk

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,6 +76,7 @@ var fight = function(enemy){
         if (playerInfo.health > 0){
         //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert(' Welcome to Robot Gladiators! Round ' + (i + 1));
+        debugger;
 
         //pick new eney to fight based on the index of the enemy.names array
         var pickedEnemyObj = enemyInfo[i];
@@ -167,6 +168,7 @@ var fight = function(enemy){
   // player information
 var playerInfo = {
   name: window.prompt("What is your robot's name?"),
+  name: getPlayerNmae(),
   health: 100,
   attack: 10,
   money: 10,
@@ -212,6 +214,14 @@ var enemyInfo = [
     attack: randomNumber(10, 14)
   }
 ];
+var getPlayerName =function(){
+    var name = "";
+    while ( name ==="" || name === null){
+        name = prompt("What is your robot's name?");
+    // console.log("Your robot's name is" + name);
+    }
+    // return name
+};
 
 console.log(enemyInfo);
 console.log(enemyInfo[0]);
